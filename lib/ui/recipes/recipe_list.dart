b4440b3b53e77ca26d503eccbb0a3c2a4f6443d9
Ini_Jones <74598414+inijones@users.noreply.github.com>
2022-10-24 10:05:03 +0100

    Implemented the Catalogue Back Button

--- lib/ui/recipes/recipe_list.dart
+++ lib/ui/recipes/recipe_list.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RecipeList extends StatefulWidget {
   const RecipeList({super.key});
@@ -9,6 +11,66 @@ class RecipeList extends StatefulWidget {
 }
 
 class _RecipeListState extends State<RecipeList> {
+  static const String prefSearchKey = 'previousSearches';
+
+  late TextEditingController searchTextController;
+  final ScrollController _scrollController = ScrollController();
+  List currentSearchList = [];
+  int currentCount = 0;
+  int currentStartPosition = 0;
+  int currentEndPosition = 20;
+  int pageCount = 20;
+  bool hasMore = true;
+  bool loading = false;
+  bool inErrorState = false;
+  List<String> previousSearches = <String>[];
+
+  // TODO: Add currentRecipes1
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: Call loadRecipes()
+    getPreviousSearches();
+    searchTextController = TextEditingController(text: '');
+    _scrollController.addListener(() {
+      final triggerFetchMoreSize =
+          0.7 * _scrollController.position.maxScrollExtent;
+
+      if (_scrollController.position.pixels > triggerFetchMoreSize) {
+        if (hasMore &&
+            currentEndPosition < currentCount &&
+            !loading &&
+            !inErrorState) {
+          setState(() {
+            loading = true;
+            currentStartPosition = currentEndPosition;
+            currentEndPosition = min(currentStartPosition + pageCount, currentCount)
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
+  void savevPreviousSearches() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList(prefSearchKey, previousSearches);
+  }
+
+  void getPreviousSearches() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey(prefSearchKey)) {
+      final searches = prefs.getString(prefSearchKey);
+      if (searches != null) {}
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
